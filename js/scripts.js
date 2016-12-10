292743e4f55bf96b5cbe1a35d4605e389873936d
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2016-12-10 14:25:40 -0800

    Latest changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
-jQuery(document).ready(function($) {
-	$('.layout-aside .tags a').each(function() {
-		$(this).wrap('<li class="tags-item"/>');
-	});
+// jQuery(document).ready(function($) {
+// 	$('.layout-aside .tags a').each(function() {
+// 		$(this).wrap('<li class="tags-item"/>');
+// 	});
 
-	$('.header-search .header-search-field').focusin(function() {
-		$(this).parent().addClass('focus');
-	}).focusout(function() {
-		$(this).parent().removeClass('focus');
-	});
+// 	$('.header-search .header-search-field').focusin(function() {
+// 		$(this).parent().addClass('focus');
+// 	}).focusout(function() {
+// 		$(this).parent().removeClass('focus');
+// 	});
 
-	$('.aside-entries-post .teaser-aside-entries:last-child').hide();
-	$('.aside-entries-post .teaser-aside-entries:visible:last').css('border-bottom', 'none');
+// 	$('.aside-entries-post .teaser-aside-entries:last-child').hide();
+// 	$('.aside-entries-post .teaser-aside-entries:visible:last').css('border-bottom', 'none');
 
-	var teaser_title_featured = $('.teaser-title-featured').text().trim();
-	var content_title = $('.layout-content-entry').find('.content-title').text().trim();
-	$('.teaser-title:not(.teaser-title-featured)').each(function() {
-		if ($(this).text().trim() == teaser_title_featured) {
-			$(this).closest('.teaser').remove();
-		} else if ($(this).text().trim() == content_title) {
-			$(this).closest('.teaser').remove();
-			$('.aside-entries-post .teaser-aside-entries:visible:last').removeAttr('style');
-			$('.aside-entries-post').find('.teaser-aside-entries:last-child').show();
-		}
-	});
+// 	var teaser_title_featured = $('.teaser-title-featured').text().trim();
+// 	var content_title = $('.layout-content-entry').find('.content-title').text().trim();
+// 	$('.teaser-title:not(.teaser-title-featured)').each(function() {
+// 		if ($(this).text().trim() == teaser_title_featured) {
+// 			$(this).closest('.teaser').remove();
+// 		} else if ($(this).text().trim() == content_title) {
+// 			$(this).closest('.teaser').remove();
+// 			$('.aside-entries-post .teaser-aside-entries:visible:last').removeAttr('style');
+// 			$('.aside-entries-post').find('.teaser-aside-entries:last-child').show();
+// 		}
+// 	});
 
-	$('.teaser-content .teaser-intro figure:hidden').remove();
-});
+// 	$('.teaser-content .teaser-intro figure:hidden').remove();
+// });
