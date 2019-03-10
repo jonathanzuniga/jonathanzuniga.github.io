dbc94d6d51534fa804a0cd55630200f5c25b4722
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2019-03-10 14:40:24 -0700

    Add the top posts inside a slider.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
+var slider = tns({
+    container: ".slider",
+    items: 1,
+    controls: false,
+    navContainer: ".slider-nav"
+});
+
 function empty() {
-    const value = document.getElementById('search__input').value;
-    if (value.trim() == '') return false;
+    const value = document.getElementById("search__input").value;
+    if (value.trim() == "") return false;
 }
