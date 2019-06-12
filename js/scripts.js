320e3e4bb64dcf12cf72a4a084b0f4b0a93113ea
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2019-06-11 17:17:52 -0700

    Agregar plugin para footnotes, actualizar patterns file.

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,19 @@ function empty() {
     const value = document.getElementById("search__input").value;
     if (value.trim() == "") return false;
 }
+
+const fragment = document.createDocumentFragment();
+fragment.appendChild(document.getElementsByClassName("footnotes")[0]);
+document.getElementsByClassName("post__notes--right")[0].appendChild(fragment);
+
+setFootNotes = () => {
+    if ($(window).width() >= 1024)
+        $(".post--full sup").footprint({
+            notes: ".footnotes li",
+            container: ".footnotes"
+        });
+};
+
+setFootNotes();
+
+$(window).resize(() => setFootNotes());
